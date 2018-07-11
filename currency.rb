5688c226c7ce3bba0bdabfc0dedcf7ea1df1a79f
shiruiuiui <shirui.us@gmail.com>
2018-07-11 14:08:25 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,8 +1,10 @@
 # Write your cod here
 def usd_to_eur
-  
+  new_currency = amount * 0.86
 end
 
-def eur_to_usd
-  
+def eur_to_usd(amount)
+  new_currency = amount * 1.16
 end
+
+def usd_to_jpy(amount)
